76094bec75a1ffa55225e7da2ab6f2a24bf50585
frogfrequency <braun.marcel@hotmail.com>
2020-10-15 15:54:44 +0200

    first wave prototype

--- readme.md
+++ readme.md
@@ -10,3 +10,14 @@ This is a repository for various mini coding-projects.
 - [pixelEstimator_V2](pixelEstimator_V2)
 - [spotlightSUM](spotlightSUM)
 - [waves](waves)
+
+
+When working on new features or tasks, create new branches with naming pattern:
+
+- task/my-current-task-description
+- feature/my-current-feature-description
+
+If you are working on a bugfix, create new branches with naming pattern:
+
+- fix/my-current-problem
+- bugfix/my-current-problem
--- waves/wave1/code.js
+++ waves/wave1/code.js
@@ -1,8 +1,21 @@
-const newDiv = document.createElement("div");
 
-console.log(newDiv);
-newDiv.textContent = "hoi du";
 
-mainDiv = document.getElementById
+function generateWave() {
 
-clg
+    const mainDiv = document.getElementById("main-div");
+    mainDiv.innerHTML = "";
+    
+    var factor = document.getElementById("inputtedSteepness").value;
+    console.log(factor);
+    for (i=0; i<100; i++) { 
+        const newDiv = document.createElement("div");
+        newDiv.className = "wave-element";
+        newDiv.style.left = (Math.sin(i*0.1)*factor + 200) + "px";
+
+
+        mainDiv.appendChild(newDiv);
+
+    }
+
+}
+
