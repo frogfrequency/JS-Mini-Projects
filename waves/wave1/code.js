1be74490c0b6e16e7a3af0890ed31cc7e55e39f9
frogfrequency <braun.marcel@hotmail.com>
2020-10-19 17:51:39 +0200

    modified wave1 with timing and colors

--- waves/wave1/code.js
+++ waves/wave1/code.js
@@ -2,18 +2,24 @@
 
 function generateWave() {
 
-    const mainDiv = document.getElementById("main-div");
+    const mainDiv = document.getElementById('main-div');
     mainDiv.innerHTML = "";
     
-    var factor = document.getElementById("inputtedSteepness").value;
+    const factor = document.getElementById("inputtedSteepness").value;
+    const desiredColor = document.getElementById("bg-color-selector").value;
+    console.log(desiredColor);
+    
     console.log(factor);
-    for (i=0; i<100; i++) { 
+    for (i=0; i<1000; i++) { 
         const newDiv = document.createElement("div");
         newDiv.className = "wave-element";
+        newDiv.style["background-color"] = desiredColor;
         newDiv.style.left = (Math.sin(i*0.1)*factor + 200) + "px";
 
 
-        mainDiv.appendChild(newDiv);
+        setTimeout(() => {
+            mainDiv.appendChild(newDiv);
+        }, 50 * i);
 
     }
 
